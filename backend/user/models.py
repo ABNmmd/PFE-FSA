a60b5e6058a0bee39046f23f2cf88c0ed3f7f76f
ABNmmd <moha.ww.ha@gmail.com>
2025-03-12 22:34:09 +0000

    Add from_dict method to User model for object creation from dictionary

--- backend/user/models.py
+++ backend/user/models.py
@@ -15,6 +15,13 @@ class User:
         self.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
         self.id = None
 
+    @classmethod
+    def from_dict(cls, data):
+        user = cls(data['username'], data['email'], data['password'])
+        user.id = str(data['_id'])
+        user.password = data['password']
+        return user
+
     def save(self):
         user_data = {
             "username": self.username,
@@ -36,10 +43,12 @@ class User:
     def get_user_by_username(username):
         user = db.users.find_one({"username": username})
         if user:
-            user_obj = User(user['username'], user['email'], user['password'])
-            user_obj.id = str(user['_id'])
+            user_obj = User.from_dict(user)
             return user_obj
         return None
 
     def check_password(self, password):
         return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
+
+    def __repr__(self):
+        return f"User(username='{self.username}', email='{self.email}', id='{self.id}')"
