f992dc2c567ffca113dc2d5b93c67ae1d8a8475e
ABNmmd <moha.ww.ha@gmail.com>
2025-03-13 23:42:29 +0000

    Add Home component with welcome message

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="min-h-screen bg-blue-100">
+      <h1 className="text-5xl font-bold text-center">
+        Welcome to Home!
+      </h1>
+    </div>
+  )
+}
+
+export default Home;
