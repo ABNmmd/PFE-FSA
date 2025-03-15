632eb115e42549043d754c62c05434d9b533ae7f
AhmedAMDICH <ahmedamdich12@gmail.com>
2025-03-15 03:53:42 +0000

    Add CORS support and update requirements for flask-cors

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
+#from flask_cors import CORS
+from flask_cors import CORS
+
 from user.routes import user_bp
 
 app = Flask(__name__)
-
+CORS(app)
 app.register_blueprint(user_bp)
 
 @app.route('/')
