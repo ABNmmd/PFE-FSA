61cdccd58de93db4b59e7f928801c67074ae6a18
ABNmmd <moha.ww.ha@gmail.com>
2025-03-20 02:03:54 +0000

    Add dotenv support and register document blueprint in app.py

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,19 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
+from dotenv import load_dotenv
 
 from user.routes import user_bp
+from documents.routes import document_bp
 
 app = Flask(__name__)
 CORS(app)
+
+load_dotenv()
+app.secret_key = os.getenv("APP_SECRET_KEY")
+
 app.register_blueprint(user_bp)
+app.register_blueprint(document_bp)
 
 @app.route('/')
 def api():
