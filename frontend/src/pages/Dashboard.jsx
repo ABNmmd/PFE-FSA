9a0457199d18a320d3a6c9e97f989b15d6b24110
ABNmmd <moha.ww.ha@gmail.com>
2025-03-30 02:50:25 +0000

    Add Alert component to Dashboard for displaying connection messages

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,14 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, use } from 'react';
 import DashboardAside from '../components/DashboardAside';
 import { MdDocumentScanner } from "react-icons/md";
 import { TbReportAnalytics } from "react-icons/tb";
 import DashboardNumbers from '../components/DashboardNumbers';
 import FilesManager from '../components/filesManager';
+import { useAuth } from '../context/AuthContext';
+import Alert from '../components/Alert';
 
 function Dashboard() {
   const [totalDocuments, setTotalDocuments] = useState(0);
   const [totalReports, setTotalReports] = useState(0);
   const [totalPlagiarismChecks, setTotalPlagiarismChecks] = useState(0);
+  const { message, setMessage, connectedToDrive } = useAuth()
+
+  useEffect(() => {
+    console.log(message);
+  }, [message]);
 
   const dashboardPages = [
     {
@@ -44,17 +51,28 @@ function Dashboard() {
   
 
   return (
-    <div className="flex h-screen bg-gray-100">
-      <DashboardAside arr={dashboardPages} />
-      <main className="flex-1 p-4">
-        <section className='grid grid-cols-3 gap-4 mb-8'>
-          <DashboardNumbers list={statistics} />
-        </section>
-        <section>
-          <FilesManager />
-        </section>
-      </main>
-    </div>
+    <>
+      <div className="flex h-screen bg-gray-100">
+        <DashboardAside arr={dashboardPages} />
+        <main className="flex-1 p-4 h-screen overflow-y-auto relative">
+          <section className='grid grid-cols-3 gap-4 mb-8'>
+            <DashboardNumbers list={statistics} />
+          </section>
+          <section>
+            <FilesManager />
+          </section>
+
+
+          {message && (
+            <Alert
+              message={message}
+              success={connectedToDrive}
+              onClose={() => setMessage(null)}
+            />
+          )}
+        </main>
+      </div>
+    </>
   )
 }
 
