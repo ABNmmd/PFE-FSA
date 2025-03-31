7ca64809ad8fff51dff04c0cc434dff40177822d
ABNmmd <moha.ww.ha@gmail.com>
2025-03-31 23:05:47 +0000

    Add ToastProvider to main.jsx for toast notifications integration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,17 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
 import { DocumentProvider } from './context/DocumentContext'
+import { ToastProvider } from './context/ToastContext'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <DocumentProvider>
-        <App />
-      </DocumentProvider>
+      <ToastProvider>
+        <DocumentProvider>
+          <App />
+        </DocumentProvider>
+      </ToastProvider>
     </AuthProvider>
   </StrictMode>,
 )
