179c9a38fbf4fbd995f30f3c009f5fb2951375f5
AhmedAMDICH <ahmedamdich12@gmail.com>
2025-04-08 22:14:42 +0100

    Add routes for creating and retrieving plagiarism reports

--- backend/reports/routes.py
+++ backend/reports/routes.py
@@ -4,28 +4,26 @@ from user.routes import token_required
 
 reports_bp = Blueprint('reports', __name__, url_prefix='/reports')
 
-@reports_bp.route('/create', methods=['POST'])
+@reports_bp.route('/', methods=['POST'])
 @token_required
 def create_plagiarism_report():
     data = request.get_json()
 
     document1 = data.get("document1")
     document2 = data.get("document2")
-    taux_similarite_global = data.get("taux_similarite_global")
-    algorithme_utilise = data.get("algorithme_utilise")
-    sections_similaires = data.get("sections_similaires", [])
-    matched_content = data.get("matched_content", [])
+    global_similarity_rate = data.get("global_similarity_rate")
+    algorithm_used = data.get("algorithm_used")
+    similar_sections = data.get("similar_sections", [])
 
-    if not document1 or not document2 or taux_similarite_global is None or not algorithme_utilise:
+    if not document1 or not document2 or global_similarity_rate is None or not algorithm_used:
         return jsonify({"message": "Missing required fields"}), 400
 
     report = PlagiarismReport(
         document1=document1,
         document2=document2,
-        taux_similarite_global=taux_similarite_global,
-        algorithme_utilise=algorithme_utilise,
-        sections_similaires=sections_similaires,
-        matched_content=matched_content
+        global_similarity_rate=global_similarity_rate,
+        algorithm_used=algorithm_used,
+        similar_sections=similar_sections
     )
     report_id = report.save()
 
@@ -39,14 +37,13 @@ def get_plagiarism_report(report_id):
         return jsonify({"message": "Report not found"}), 404
 
     formatted_report = {
-        "_id": report["_id"],
-        "date_generation": report["date_generation"].isoformat() + "Z",
-        "document1": report["document1"],
-        "document2": report["document2"],
-        "taux_similarite_global": report["taux_similarite_global"],
-        "algorithme_utilise": report["algorithme_utilise"],
-        "sections_similaires": report["sections_similaires"],
-        "matched_content": report["matched_content"]
+        "_id": report.id,
+        "date_generated": report.date_generated.isoformat() + "Z",
+        "document1": report.document1,
+        "document2": report.document2,
+        "global_similarity_rate": report.global_similarity_rate,
+        "algorithm_used": report.algorithm_used,
+        "similar_sections": report.similar_sections,
     }
 
     return jsonify(formatted_report), 200
