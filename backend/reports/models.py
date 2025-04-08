74282678328106a891e14ebc65f9e43a4bb396e3
AhmedAMDICH <ahmedamdich12@gmail.com>
2025-04-08 22:13:59 +0100

    Add PlagiarismReport model for managing plagiarism report data

--- backend/reports/models.py
+++ backend/reports/models.py
@@ -5,31 +5,44 @@ from bson import ObjectId
 db = get_db()
 
 class PlagiarismReport:
-    def __init__(self, document1, document2, taux_similarite_global, algorithme_utilise, sections_similaires, matched_content):
+    def __init__(self, document1, document2, global_similarity_rate, algorithm_used, similar_sections, date_generated=None):
         self.document1 = document1
         self.document2 = document2
-        self.taux_similarite_global = taux_similarite_global
-        self.algorithme_utilise = algorithme_utilise
-        self.sections_similaires = sections_similaires
-        self.matched_content = matched_content
-        self.date_generation = datetime.utcnow()
+        self.global_similarity_rate = global_similarity_rate
+        self.algorithm_used = algorithm_used
+        self.similar_sections = similar_sections
+        self.date_generated = date_generated or datetime.utcnow()
+        self.id = None
+
+    @classmethod
+    def from_dict(cls, data):
+        report = cls(
+            document1=data.get("document1"),
+            document2=data.get("document2"),
+            global_similarity_rate=data.get("global_similarity_rate"),
+            algorithm_used=data.get("algorithm_used"),
+            similar_sections=data.get("similar_sections", []),
+            date_generated=data.get("date_generated")
+        )
+        report.id = str(data["_id"])
+        return report
 
     def save(self):
         report_data = {
             "document1": self.document1,
             "document2": self.document2,
-            "taux_similarite_global": self.taux_similarite_global,
-            "algorithme_utilise": self.algorithme_utilise,
-            "sections_similaires": self.sections_similaires,
-            "matched_content": self.matched_content,
-            "date_generation": self.date_generation
+            "global_similarity_rate": self.global_similarity_rate,
+            "algorithm_used": self.algorithm_used,
+            "similar_sections": self.similar_sections,
+            "date_generated": self.date_generated
         }
         result = db.plagiarism_reports.insert_one(report_data)
-        return str(result.inserted_id)
+        self.id = str(result.inserted_id)
+        return self.id
 
     @staticmethod
     def get_report_by_id(report_id):
         report = db.plagiarism_reports.find_one({"_id": ObjectId(report_id)})
         if report:
-            report["_id"] = str(report["_id"])
-        return report
+            return PlagiarismReport.from_dict(report)
+        return None
